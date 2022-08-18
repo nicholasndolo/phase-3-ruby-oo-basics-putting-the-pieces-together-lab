a886b3be73500ac2d8a3af749eaf7c57d35fe83d
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-18 22:02:55 +0300

    Lab Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
+
+book = Book.new("Ruby Fundamentals")
+book.author = "Agatha Christie"
+book.page_count = "272"
+book.genre = "Mystery"
+
+book = Book.new("The World According to Garp")
+book.turn_page
 
 
